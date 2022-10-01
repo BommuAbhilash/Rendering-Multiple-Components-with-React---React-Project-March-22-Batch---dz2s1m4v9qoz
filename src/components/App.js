7e7f85eaac63baf5f59ed0d205c9cfd25a8618b4
BommuAbhilash <bommuabhi.ba@gmail.com>
2022-10-01 12:15:01 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Travel Explorer</h1>
+      <h2 data-ns-test="project-description">Services for the people who love to explore India by guiding them</h2>
+    </div>
   )
 }
 
